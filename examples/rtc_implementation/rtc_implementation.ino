3a2eb3125fa4c6dd46ce0c0808a800c035e11e9e
S-Panta <sabinpanta11@gmail.com>
2026-07-21 18:52:13 -0600

    rtc without digixbee client

--- examples/rtc_implementation/rtc_implementation.ino
+++ examples/rtc_implementation/rtc_implementation.ino
@@ -1,35 +1,29 @@
 #include <WiFiS3.h>
 
-
-const char* ssid = "USU-guest";
-const char* password = 0;
+const char *ssid = "USU-guest";
+const char *password = 0;
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
 #include "RTC.h"
 WiFiUDP ntpUDP;
 
-// 60 millisecond for update frequency 
+// 60 millisecond for update frequency
 // setUpdateInterval can be used to change later
-NTPClient timeClient(ntpUDP, "north-america.pool.ntp.org", -6 * 3600,60000);
+NTPClient timeClient(ntpUDP, "north-america.pool.ntp.org", -6 * 3600, 60000);
 
 String getISO8601Time(RTCTime &t) {
   char buf[25];
-  snprintf(buf, sizeof(buf), "%04d-%02d-%02dT%02d:%02d:%02dZ",
-           t.getYear(),
-           Month2int(t.getMonth()),
-           t.getDayOfMonth(),
-           t.getHour(),
-           t.getMinutes(),
-           t.getSeconds());
+  snprintf(buf, sizeof(buf), "%04d-%02d-%02dT%02d:%02d:%02dZ", t.getYear(),
+           Month2int(t.getMonth()), t.getDayOfMonth(), t.getHour(),
+           t.getMinutes(), t.getSeconds());
   return String(buf);
 }
-
 
 void connectWiFi() {
   delay(2000);
   Serial.print("Connecting to WiFi: ");
-  WiFi.begin(ssid,password);
+  WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -58,7 +52,6 @@ void setup() {
   timeClient.update();
   RTC.begin();
   syncRTCFromNTP();
-
 }
 
 void loop() {
