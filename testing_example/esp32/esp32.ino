336ba0f86ba0a30d5124be51eb684b5e61687f85
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2026-07-22 17:08:27 -0600

    esp32 wifi connection (#13)
    * esp32 wifi connection
    
    * esp32 AT command for debug
    
    * mqtt for esp32
    
    * http post to local hydroserver
    
    * https request to hydroserver

--- testing_example/esp32/esp32.ino
+++ testing_example/esp32/esp32.ino
@@ -1,46 +1,139 @@
+#include <Arduino.h>
+
+#define TINY_GSM_MODEM_ESP32
 #define XbeeSerial Serial1
+// #define TINY_GSM_DEBUG Serial
+#include <TinyGsmClient.h>
 #define XBEE_PWR 18
 
-long bauds[] = {9600, 19200, 38400, 57600, 115200};
-int numBauds = 5;
+
+
+
+#define TINY_GSM_USE_GPRS false
+#define TINY_GSM_USE_WIFI true
+
+#include "arduino_secrets.h"
+#include <HydroServerMQTTClient.h>
+
+
+// wifi details
+const char* wifiId  = WIFI_SSID;
+const char* wifiPwd = WIFI_PASS;
+
+
+const char *MQTT_BROKER = "raspberrypi1.mypc.usu.edu";
+const int32_t modemBaud     = 57600;
+
+// #include <StreamDebugger.h>
+// StreamDebugger debugger(Serial1, Serial);
+// TinyGsm        modem(debugger);
+
+TinyGsm modem(XbeeSerial);
+TinyGsmClient client(modem);
+
+HydroServerMQTTClient mqttClient(client, MQTT_BROKER);
+
+Observation temperature;
+
+String sendATCommand(String cmd, uint32_t timeout_ms = 2000) {
+  while (XbeeSerial.available()) {
+    XbeeSerial.read();
+  }
+
+  Serial.print(">> ");
+  Serial.println(cmd);
+
+  XbeeSerial.print(cmd);
+  XbeeSerial.print("\r\n");
+
+  String response = "";
+  uint32_t start = millis();
+  while (millis() - start < timeout_ms) {
+    while (XbeeSerial.available()) {
+      char c = XbeeSerial.read();
+      response += c;
+      start = millis(); 
+    }
+    if (response.endsWith("OK\r\n") || response.endsWith("ERROR\r\n")) {
+      break;
+    }
+  }
+
+  Serial.print("<< ");
+  Serial.println(response);
+  Serial.println("....................................");
+
+  return response;
+}
+
+void connectWiFi() {
+  if (!modem.waitForNetwork()) {
+    Serial.println("Wifi is not connected");
+    delay(10000);
+    return;
+  }
+
+  Serial.println("Wifi is connected");
+
+  if (modem.isNetworkConnected()) { Serial.println("Network connected"); }
+
+  Serial.print("Local IP: ");
+  Serial.println(modem.localIP());
+  // sendATCommand("AT+CIPMUX=1");
+  // Serial.println("CIPMUX is set to 1 in this step");
+
+  // sendATCommand("AT+CIPMUX=1");
+  // delay(2000);
+  // sendATCommand("AT+CIPRECVMODE?");
+  // This is important to make sure your mqtt works with esp32
+  sendATCommand("AT+CIPRECVMODE=1");
+  Serial.println("the firmware set to passive");
+  // delay(3000);
+  // sendATCommand("AT+CIPRECVMODE?");
+  // Serial.println("checking again");
+  // sendATCommand("AT+CIPSTART=0,\"TCP\",\"test.mosquitto.org\",1883");
+  // Serial.println("tcp connection is openeed in this step");
+}
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+  Serial.println("esp32 test");
+
   pinMode(XBEE_PWR, OUTPUT);
   digitalWrite(XBEE_PWR, HIGH);
-  delay(2000);
-  Serial.println("Power stabilized, starting baud scan...");
+
+  XbeeSerial.begin(57600); 
+
+  delay(3000);               
+  Serial.println("powered up module");
+  modem.init();
+
+  connectWiFi();
+  Serial.println("Now broker connection step has started");
+  mqttClient.setSiteCode("uwrl");
+  mqttClient.setClientID("Arduinopublisher");
+  if(!mqttClient.connectToBroker()){
+    Serial.println("Cannot connect to Broker. Connection Error is ");
+    Serial.println(mqttClient.getConnectionError());
+    // it make no sense to work further when connection to broker is not successful
+    while (1);
+  };
+
+  Serial.println("connection successful");
+  temperature.datastreamId = "data-streamid";
+  temperature.sensorId = "sensorid";
+  temperature.observedProperty = "temperature_celsius";
 }
+
 
 void loop() {
-  for (int i = 0; i < numBauds; i++) {
-    Serial.print("\n--- Trying baud: ");
-    Serial.println(bauds[i]);
-
-    XbeeSerial.begin(bauds[i]);
-    delay(200);
-    while (XbeeSerial.available()) XbeeSerial.read();
-
-    XbeeSerial.print("AT\r\n");
-    delay(500);
-
-    Serial.print("Response (hex): ");
-    if (XbeeSerial.available()) {
-      while (XbeeSerial.available()) {
-        uint8_t c = (uint8_t)XbeeSerial.read();  
-        Serial.print(c, HEX);
-        Serial.print(' ');
-      }
-    } else {
-      Serial.print("(nothing)");
-    }
-    Serial.println();
-
-    XbeeSerial.end();
-    delay(300);
-  }
-
-  Serial.println("\n=== Scan complete, repeating in 5s ===\n");
-  delay(5000);
+  mqttClient.poll();
+  float randomTemp;
+  randomTemp = random(20,25);
+  temperature.value = randomTemp;
+  mqttClient.publishObservation(temperature, "2026-06-15T00:00:00Z");
+  delay(30000);
 }
+
+
---
+++ testing_example/esp32_AT_debug/esp32_AT_debug.ino
@@ -0,0 +1,78 @@
+#include <Arduino.h>
+
+#define XbeeSerial Serial1
+#define XBEE_PWR 18
+
+const int32_t modemBaud = 57600;
+
+String sendATCommand(String cmd, uint32_t timeout_ms = 2000) {
+  while (XbeeSerial.available()) {
+    XbeeSerial.read();
+  }
+
+  Serial.print(">> ");
+  Serial.println(cmd);
+
+  XbeeSerial.print(cmd);
+  XbeeSerial.print("\r\n");
+
+  String response = "";
+  uint32_t start = millis();
+  while (millis() - start < timeout_ms) {
+    while (XbeeSerial.available()) {
+      char c = XbeeSerial.read();
+      response += c;
+      start = millis(); 
+    }
+    if (response.endsWith("OK\r\n") || response.endsWith("ERROR\r\n")) {
+      break;
+    }
+  }
+
+  Serial.print("<< ");
+  Serial.println(response);
+  Serial.println("....................................");
+
+  return response;
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("esp32 AT command test");
+
+  pinMode(XBEE_PWR, OUTPUT);
+  digitalWrite(XBEE_PWR, HIGH);
+
+  XbeeSerial.begin(modemBaud);
+  delay(3000);
+  Serial.println("powered up module");
+
+  sendATCommand("AT");
+// sendATCommand("AT+GMR");
+// sendATCommand("AT+CIPMUX?");
+// sendATCommand("AT+CIPSTATUS");
+// sendATCommand("AT+CMD?");
+// sendATCommand("AT+CIPSTA?");
+// delay(2000);
+sendATCommand("AT+CIPRECVMODE?");
+sendATCommand("AT+CIPMUX?");
+// sendATCommand("AT+GMR");
+// sendATCommand("AT+CIPMUX=1");
+// sendATCommand("AT+CIPMUX?");
+// sendATCommand("AT+CIPSTART=0,\"TCP\",\"test.mosquitto.org\",1883");
+// // sendATCommand("AT+CIPSTART=\"TCP\",\"test.mosquitto.org\",1883");
+// sendATCommand("AT+CIPSTATUS");
+// sendATCommand("AT+CIPMODE?");
+// sendATCommand("AT+PING=\"192.168.0.103\"");
+// sendATCommand("AT+CIPSTAMAC?");
+
+  // sendATCommand("AT+GMR");
+  // sendATCommand("AT+CWMODE?");
+  // sendATCommand("AT+CIPSTA?");
+
+  Serial.println("done");
+}
+
+void loop() {
+}
