d98f87008c328e5b5aa1aaa787fdbb3c79ca57fc
S-Panta <sabinpanta11@gmail.com>
2026-07-21 22:07:27 -0600

    esp32 wifi connection

--- testing_example/esp32/esp32.ino
+++ testing_example/esp32/esp32.ino
@@ -1,46 +1,74 @@
+#include <Arduino.h>
+
+#define TINY_GSM_MODEM_ESP32
 #define XbeeSerial Serial1
+#include <TinyGsmClient.h>
 #define XBEE_PWR 18
 
-long bauds[] = {9600, 19200, 38400, 57600, 115200};
-int numBauds = 5;
+
+#define TINY_GSM_USE_WIFI true
+
+#include "arduino_secrets.h"
+
+// wifi details
+const char* wifiId  = WIFI_SSID;
+const char* wifiPwd = WIFI_PASS;
+
+TinyGsm modem(XbeeSerial);
+TinyGsmClient client(modem);
+
+
+const int32_t modemBaud     = 57600;
+
+Observation temperature = {"temperature", "uuidtemperature", "tempsensorid"};
+
+
+void connectWiFi() {
+  if (!modem.waitForNetwork()) {
+    Serial.println("Wifi is not connected");
+    delay(10000);
+    return;
+  }
+
+  Serial.println("Wifi is connected");
+
+  if (modem.isNetworkConnected()) { Serial.println("Network connected"); }
+
+  Serial.print("Local IP: ");
+  Serial.println(modem.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+  Serial.println("esp32 test");
+
   pinMode(XBEE_PWR, OUTPUT);
   digitalWrite(XBEE_PWR, HIGH);
-  delay(2000);
-  Serial.println("Power stabilized, starting baud scan...");
+
+  XbeeSerial.begin(57600); 
+
+  // modem.modemPowerUp();
+  delay(3000);               
+  Serial.println("powered up module");
+
+  connectWiFi(); 
+  mqttClient.setSiteCode("uwrl");
+  mqttClient.setClientID("Arduinopublisher"); 
+
+
+  if(!mqttClient.connectToBroker()){
+    Serial.println("Cannot connect to Broker. Connection Error is ");
+    Serial.println(mqttClient.getConnectionError());
+    // it make no sense to work further when connection to broker is not successful
+    while (1);
+  };
+
+  Serial.println("connection successful");  
 }
 
-void loop() {
-  for (int i = 0; i < numBauds; i++) {
-    Serial.print("\n--- Trying baud: ");
-    Serial.println(bauds[i]);
 
-    XbeeSerial.begin(bauds[i]);
-    delay(200);
-    while (XbeeSerial.available()) XbeeSerial.read();
-
-    XbeeSerial.print("AT\r\n");
-    delay(500);
-
-    Serial.print("Response (hex): ");
-    if (XbeeSerial.available()) {
-      while (XbeeSerial.available()) {
-        uint8_t c = (uint8_t)XbeeSerial.read();  
-        Serial.print(c, HEX);
-        Serial.print(' ');
-      }
-    } else {
-      Serial.print("(nothing)");
-    }
-    Serial.println();
-
-    XbeeSerial.end();
-    delay(300);
-  }
-
-  Serial.println("\n=== Scan complete, repeating in 5s ===\n");
-  delay(5000);
+void loop() { 
 }
+
+
